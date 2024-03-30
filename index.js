98eb0f11dc020624e51018cd14284b364b699747
shadrack-star <shaddkiplangat98@gmail.com>
2024-03-30 13:37:18 +0300

    performing rithmethic operations

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor( Math.random() * 1000 ) + 1;
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max( 5, 10, 15, 20 );
+  
